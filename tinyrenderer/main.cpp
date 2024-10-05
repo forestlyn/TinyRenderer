7e049d2ad8c9e01a02337ffd4120dd7327f02b0e
forestlyn <yang2797817213@qq.com>
2024-10-05 12:38:09 +0800

    add light

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -118,13 +118,18 @@ int main(int argc, char **argv)
 		;
 
 	TGAImage image(width, height, TGAImage::RGB);
+	Vec3f light_dir(0, 0, -1); // define light_dir
+
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
 		Vec3f pts[3];
 		for (int i = 0; i < 3; i++)
 			pts[i] = world2screen(model->vert(face[i]));
-		triangle(pts[0], pts[1], pts[2], zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		Vec3f n = cross(model->vert(face[2]) - model->vert(face[0]), model->vert(face[1]) - model->vert(face[0]));
+		n.normalize();
+		float intensity = n * light_dir;
+		triangle(pts[0], pts[1], pts[2], zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
 	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
