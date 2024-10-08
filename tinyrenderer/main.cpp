a993af4e479153e4f27412c0507968c33d3a9870
YangYulin <2797817213@qq.com>
2024-10-08 13:59:18 +0800

    implement model view to camera view

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -17,8 +17,10 @@ const int width = 800;
 const int height = 800;
 const int depth = 255;
 
-Vec3f camera(0, 0, 3);
-Vec3f lightdir(0, 0, -1);
+Vec3f camera(1, 1, 3);
+Vec3f center(0, 0, 0);
+
+Vec3f lightdir = Vec3f(1, -1, 1).normalize();
 
 Vec3f m2v(Matrix m)
 {
@@ -46,6 +48,24 @@ Matrix viewport(int x, int y, int w, int h)
 	m[1][1] = h / 2.f;
 	m[2][2] = depth / 2.f;
 	return m;
+}
+
+Matrix lookat(Vec3f eye, Vec3f center, Vec3f up)
+{
+	Vec3f z = (eye - center).normalize();
+	Vec3f x = (up ^ z).normalize();
+	Vec3f y = (z ^ x).normalize();
+	Matrix subO = Matrix::identity(4);
+	Matrix view = Matrix::identity(4);
+	for (int i = 0; i < 3; i++)
+	{
+		view[0][i] = x[i];
+		view[1][i] = y[i];
+		view[2][i] = z[i];
+		subO[i][3] = -eye[i];
+	}
+	Matrix ModelView = view * subO;
+	return ModelView;
 }
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -97,7 +117,7 @@ Vec3f barycentric(Vec3f a, Vec3f b, Vec3f c, Vec3f p)
 	return barycentric(b - a, c - a, a - p);
 }
 
-void triangle(Vec3f *pts, Vec3f *uvs, float *zbuffer, TGAImage &image, TGAImage &uvImage, float intensity)
+void triangle(Vec3f *pts, Vec2i *uvs, float *zbuffer, TGAImage &image, float *intensities)
 {
 	float minx = std::min(pts[0].x, pts[1].x);
 	minx = std::min(minx, pts[2].x);
@@ -127,10 +147,11 @@ void triangle(Vec3f *pts, Vec3f *uvs, float *zbuffer, TGAImage &image, TGAImage 
 				// printf("%f %f\n", zbuffer[int(x + y * width)], z);
 				zbuffer[int(x + y * width)] = z;
 				TGAColor color;
-				Vec3f uv = uvs[0] * res.x + uvs[1] * res.y + uvs[2] * res.z;
-				color = uvImage.get(uv[0] * uvImage.get_width(), uv[1] * uvImage.get_height());
-				// printf("%f %f %f\n", uv.x, uv.y, uv.z);
-				// printf("%f %f %f\n", uvs[0].x, uvs[0].y, uvs[0].z);
+				Vec2i uv = uvs[0] * res.x + uvs[1] * res.y + uvs[2] * res.z;
+				float intensity = intensities[0] * res.x + intensities[1] * res.y + intensities[2] * res.z;
+				color = model->diffuse(uv);
+				//  printf("%f %f %f\n", uv.x, uv.y, uv.z);
+				//  printf("%f %f %f\n", uvs[0].x, uvs[0].y, uvs[0].z);
 				image.set(int(x), int(y), color * intensity);
 			}
 		}
@@ -156,38 +177,32 @@ int main(int argc, char **argv)
 		;
 
 	TGAImage image(width, height, TGAImage::RGB);
-	TGAImage uvimage = TGAImage();
-	bool readSuccess = uvimage.read_tga_file("obj/african_head_diffuse.tga");
-	// have the origin at the left top corner of the image
-	uvimage.flip_vertically();
 	Matrix Perspective = Matrix::identity(4);
-	Perspective[3][2] = -1. / camera.z;
-	Matrix view = viewport(0, 0, width, height);
-	if (readSuccess)
-	{
-		for (int i = 0; i < model->nfaces(); i++)
-		{
-			std::vector<int> face = model->face(i);
-			Vec3f pts[3];
-			Vec3f uvs[3];
-			Vec3f world_verts[3];
-			for (int i = 0; i < 3; i++)
-			{
-				Vec3f v = model->vert(face[i * 2]);
-				world_verts[i] = v;
-				Vec3f temp = m2v(view * (Perspective * v2m(v)));
-				pts[i] = Vec3f(int(temp.x), int(temp.y), temp.z);
-				// pts[i] = temp;
-				uvs[i] = model->texture(face[i * 2 + 1]);
-			}
-			Vec3f n = (world_verts[2] - world_verts[0]) ^ (world_verts[1] - world_verts[0]);
-			n.normalize();
-			float intensity = n * lightdir;
-			if (intensity > 0)
-				triangle(pts, uvs, zbuffer, image, uvimage, intensity);
-		}
-	}
+	Perspective[3][2] = -1. / (camera - center).norm();
+	Matrix view = viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+	Matrix ModelView = lookat(camera, center, Vec3f(0, 1, 0));
 
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		Vec3f pts[3];
+		Vec2i uvs[3];
+		Vec3f world_verts[3];
+		float intensities[3];
+		for (int j = 0; j < 3; j++)
+		{
+			Vec3f v = model->vert(face[j]);
+			world_verts[j] = v;
+			Vec3f temp = m2v(view * (Perspective * ModelView * v2m(v)));
+			pts[j] = Vec3f(int(temp.x), int(temp.y), temp.z);
+			uvs[j] = model->uv(i, j);
+			intensities[j] = model->norm(i, j) * lightdir;
+		}
+		Vec3f n = (world_verts[2] - world_verts[0]) ^ (world_verts[1] - world_verts[0]);
+		n.normalize();
+		triangle(pts, uvs, zbuffer, image, intensities);
+	}
+	printf("end");
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	delete model;
