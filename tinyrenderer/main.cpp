cc3aa813c403f382b8cca17af8b0b8ad34152b85
forestlyn <yang2797817213@qq.com>
2024-11-06 20:16:20 +0800

    tangent with error

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -10,92 +10,58 @@ Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
-Vec3f light_dir = Vec3f(1, 1, 1).normalize();
-Vec3f camera(0.5, 1, 2);
-Vec3f center(0, 0, 1);
+Vec3f light_dir(1, 1, 1);
+Vec3f camera(1, 1, 3);
+Vec3f center(0, 0, 0);
 Vec3f up(0, 1, 0);
 
-struct GouraudShader : public IShader
+struct Shader : public IShader
 {
-	Vec3f varying_intensity;	 // written by vertex shader, read by fragment shader
-	mat<2, 3, float> varying_uv; // same as above
+	mat<2, 3, float> varying_uv;  // triangle uv coordinates, written by the vertex shader, read by the fragment shader
+	mat<4, 3, float> varying_tri; // triangle coordinates (clip coordinates), written by VS, read by FS
+	mat<3, 3, float> varying_nrm; // normal per vertex to be interpolated by FS
+	mat<3, 3, float> ndc_tri;	  // triangle in normalized device coordinates
 
 	virtual Vec4f vertex(int iface, int nthvert)
 	{
 		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-		// printf("%f %f %f %f ", gl_Vertex[0], gl_Vertex[1], gl_Vertex[2], gl_Vertex[3]);
-		//  read the vertex from .obj file
-		gl_Vertex = Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
-		// gl_Vertex = Viewport * gl_Vertex;													   // transform it to screen coordinates
-		varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir); // get diffuse lighting intensity
-		// printf("%f %f %f %f\n", gl_Vertex[0], gl_Vertex[1], gl_Vertex[2], gl_Vertex[3]);
+		varying_nrm.set_col(nthvert, proj<3>((Projection * ModelView).invert_transpose() * embed<4>(model->normal(iface, nthvert), 0.f)));
+		Vec4f gl_Vertex = Projection * ModelView * embed<4>(model->vert(iface, nthvert));
+		varying_tri.set_col(nthvert, Viewport * gl_Vertex);
+		ndc_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
 		return gl_Vertex;
 	}
 
 	virtual bool fragment(Vec3f bar, TGAColor &color)
 	{
-		float intensity = varying_intensity * bar; // interpolate intensity for the current pixel
-		// printf("%f\n", intensity);
+		Vec3f bn = (varying_nrm * bar).normalize();
 		Vec2f uv = varying_uv * bar;
-		color = model->diffuse(uv) * intensity; // well duh
-		return false;							// no, we do not discard this pixel
-	}
-};
 
-struct Shader : public IShader
-{
-	mat<2, 3, float> varying_uv;  // same as above
-	mat<4, 4, float> uniform_M;	  //  Projection*ModelView
-	mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+		mat<3, 3, float> A;
+		A[0] = ndc_tri.col(1) - ndc_tri.col(0);
+		A[1] = ndc_tri.col(2) - ndc_tri.col(0);
+		A[2] = bn;
 
-	virtual Vec4f vertex(int iface, int nthvert)
-	{
-		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
-		return Viewport * Projection * ModelView * gl_Vertex;	 // transform it to screen coordinates
-	}
+		mat<3, 3, float> AI = A.invert();
 
-	virtual bool fragment(Vec3f bar, TGAColor &color)
-	{
-		Vec2f uv = varying_uv * bar; // interpolate uv for the current pixel
-		Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
-		Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
-		float intensity = std::max(0.f, n * l);
-		color = model->diffuse(uv) * intensity; // well duh
-		return false;							// no, we do not discard this pixel
-	}
-};
+		Vec3f i = AI * Vec3f(varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0);
+		Vec3f j = AI * Vec3f(varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0);
 
-struct PhongShader : public IShader
-{
-	mat<2, 3, float> varying_uv;  // same as above
-	mat<4, 4, float> uniform_M;	  //  Projection*ModelView
-	mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+		mat<3, 3, float> B;
+		B.set_col(0, i.normalize());
+		B.set_col(1, j.normalize());
+		B.set_col(2, bn);
 
-	virtual Vec4f vertex(int iface, int nthvert)
-	{
-		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
-		return Viewport * Projection * ModelView * gl_Vertex;	 // transform it to screen coordinates
-	}
+		Vec3f n = (B * model->normal(uv)).normalize();
 
-	virtual bool fragment(Vec3f bar, TGAColor &color)
-	{
-		Vec2f uv = varying_uv * bar;
-		// Vec3f n = proj<3>(embed<4>(model->normal(uv))).normalize();
-		Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
-		Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
-		Vec3f r = (n * (n * l * 2.f) - l).normalize(); // reflected light
-		float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
-		float diff = std::max(0.f, n * l);
-		TGAColor c = model->diffuse(uv);
-		color = c;
-		for (int i = 0; i < 3; i++)
-			color[i] = std::min<float>(1 + c[i] * (diff + .6 * spec), 255);
+		float diff = std::max(0.f, n * light_dir);
+
+		color = model->diffuse(uv) * diff;
+
 		return false;
 	}
 };
+
 int main(int argc, char **argv)
 {
 	if (2 == argc)
@@ -104,29 +70,35 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		model = new Model("obj/floor.obj");
+		model = new Model("obj/african_head/african_head.obj");
 		// model = new Model("obj/diablo3_pose/diablo3_pose.obj");
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
 	TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 	// GouraudShader shader = GouraudShader();
-	PhongShader shader = PhongShader();
+	Shader shader = Shader();
 
 	viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
 	lookat(camera, center, up);
 	projection(-1. / (camera - center).norm());
-	shader.uniform_M = Projection * ModelView;
-	shader.uniform_MIT = (Projection * ModelView).invert_transpose();
+	light_dir = proj<3>((Projection * ModelView * embed<4>(light_dir, 0.f))).normalize();
+
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		Vec4f screen_coords[3];
 		for (int j = 0; j < 3; j++)
 		{
-			screen_coords[j] = shader.vertex(i, j);
+			shader.vertex(i, j);
+		}
+		mat<3, 4, float> pts = shader.varying_tri.transpose();
+		for (int j = 0; j < 3; j++)
+		{
+			screen_coords[j] = pts[j];
 		}
 		triangle(screen_coords, shader, image, zbuffer);
 	}
+
 	printf("end");
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
