77d50af2c4e44e1533284c0b52240c42ab66a0cd
forestlyn <yang2797817213@qq.com>
2025-02-16 15:43:57 +0800

    run it

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,43 +1,48 @@
 #include <GL/glu.h>
-#include <GL/glut.h>
+#include "GL/glut.h"
 #include <vector>
 #include <cmath>
 
-const int SCREEN_WIDTH  = 1024;
+const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 1024;
-const float camera[]           = {.6,0,1};
-const float light0_position[4] = {1,1,1,0};
+const float camera[] = {.6, 0, 1};
+const float light0_position[4] = {1, 1, 1, 0};
 
-void render_scene(void) {
+void render_scene(void)
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(camera[0], camera[1], camera[2], 0,  0, 0, 0, 1, 0);
+	gluLookAt(camera[0], camera[1], camera[2], 0, 0, 0, 0, 1, 0);
 	glColor3f(.8, 0., 0.);
 	glutSolidTeapot(.7);
 	glutSwapBuffers();
 }
 
-void process_keys(unsigned char key, int x, int y) {
-	if (27==key) {
+void process_keys(unsigned char key, int x, int y)
+{
+	if (27 == key)
+	{
 		exit(0);
 	}
 }
 
-void change_size(int w, int h) {
+void change_size(int w, int h)
+{
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-    glViewport(0, 0, w, h);
-	glOrtho(-1,1,-1,1,-1,8);
+	glViewport(0, 0, w, h);
+	glOrtho(-1, 1, -1, 1, -1, 8);
 	glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowPosition(100,100);
+	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	glutCreateWindow("GLSL tutorial");
-	glClearColor(0.0,0.0,1.0,1.0);
+	glClearColor(0.0, 0.0, 1.0, 1.0);
 
 	glutDisplayFunc(render_scene);
 	glutReshapeFunc(change_size);
@@ -52,4 +57,3 @@ int main(int argc, char **argv) {
 	glutMainLoop();
 	return 0;
 }
-
